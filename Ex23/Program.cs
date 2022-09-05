93081b7d94d409d34ed499c4b9f6930837448cc2
o.rudikova <rudikovaov@gmail.com>
2022-09-05 20:26:19 +0300

    Урок 3. Задача 23. Решение

--- Ex23/Program.cs
+++ Ex23/Program.cs
@@ -1,6 +1,7 @@
-﻿Console.Clear();
+﻿//Задача №23. 
+Console.Clear();
 Console.WriteLine("Введите число");
 int N = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i  <= N; i++){
-Console.WriteLine(i * i * i);
+    Console.WriteLine(i * i * i);
 }
