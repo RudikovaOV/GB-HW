fc53611b342e6469c1c43a449fa6967209387a65
o.rudikova <rudikovaov@gmail.com>
2022-09-05 22:45:54 +0300

    Урок 6. Задача 41. Решение

---
+++ Ex41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+int M = 5;
+Console.WriteLine("Введите " + M + "чисел");
+int k = 0;
+for(int i = 0; i < M; i++)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0)
+    {
+        k = k + 1;
+    }
+}
+Console.WriteLine("Количество чисел >0 = " + k);
+
+
+
+
