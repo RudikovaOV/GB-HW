f23c1684ee2e42a2657668e5442e7d5b9149d9d5
o.rudikova <rudikovaov@gmail.com>
2022-09-05 22:38:08 +0300

    Урок 5. Задача 34. Решение

---
+++ Ex34/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+Random rnd = new Random();
+int[] arr = new int[10];
+int k = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(100, 1000);
+    if (arr[i] % 2 == 0)
+    {
+        k = k + 1;
+    }
+}
+//для вывода на экран
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Количество четных = " + k);
