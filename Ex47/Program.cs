ec51ff51fd5f1b5214bb0764dc866d75ca688456
o.rudikova <rudikovaov@gmail.com>
2022-09-05 22:56:21 +0300

    Урок 7. Задача 47. Решение

---
+++ Ex47/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+int n = 4;
+int m = 3;
+double[,] matrix = new double[m, n];
+Random Random = new Random();
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine("i = " + i); 
+    for(int j = 0; j < n; j++)
+    {
+        Console.WriteLine("j = " + j); 
+        matrix[i, j] = Random.NextDouble() * 10;
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
