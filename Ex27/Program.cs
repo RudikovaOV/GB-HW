3adb4a4e7bd182a4690faf6a0298a7f7ea18eafa
o.rudikova <rudikovaov@gmail.com>
2022-09-05 22:30:23 +0300

    Урок 4. Задача 27. Решение

---
+++ Ex27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+int data = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (data != 0)
+{
+  int zifra = data % 10;
+  sum = sum + zifra;
+  data = data / 10;
+}
+Console.WriteLine(sum);
