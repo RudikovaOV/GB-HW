ab7ca9cf7866bc885f0e6877e4994338c9f5f058
o.rudikova <rudikovaov@gmail.com>
2022-09-10 09:09:37 +0300

    Добавлена программа

--- ProvRab/Program.cs
+++ ProvRab/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] strings = new string[6] {"Hello", "2", "World", ":-)", "55555", "dom"};
+
+int k = 0;
+
+for(int i = 0; i < strings.Length; i++)
+{
+    if (strings[i].Length <= 3)
+    {
+        k++;
+    }
+}
+
+string[] new_strings = new string[k];
+
+int j = 0;
+for(int i = 0; i < strings.Length; i++)
+{
+    if (strings[i].Length <= 3)
+    {
+        new_strings[j] = strings[i];
+        j++;
+    }
+}
+
+//вывод на экран
+for(int i = 0; i < new_strings.Length; i++)
+{
+    Console.WriteLine(new_strings[i]);
+}
+
